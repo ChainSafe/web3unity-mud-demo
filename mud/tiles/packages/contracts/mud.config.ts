89d90b9ae436ba4d327f6dda652a65846b2a4afa
viatrix <viatrix.x@gmail.com>
2024-08-20 19:31:33 +0300

    WIP: initial

--- mud/tiles/packages/contracts/mud.config.ts
+++ mud/tiles/packages/contracts/mud.config.ts
@@ -9,24 +9,5 @@ export default defineWorld({
       },
       key: [],
     },
-    Tiles: {
-      schema: {
-        id: "uint256",
-        x: "uint256",
-        y: "uint256",
-        building: "uint8"
-      },
-      key: ["id"],
-    },
-    Owners: {
-      schema: {
-          ownerAddress: "address",
-          rate: "uint256",
-          lastUpdateTime: "uint256",
-          unclaimed: "uint256",
-          tileIds: "uint256[]"
-        },
-        key: ["ownerAddress"],
-    }
   },
 });
